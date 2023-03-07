a67234874f2624170a901d1ae529b0bf29bc01af
Olenakas <121051082+Olenakas@users.noreply.github.com>
2023-03-07 19:25:45 +0100

    07.03.2023

--- js/more.js
+++ js/more.js
@@ -1,16 +1,16 @@
-function readMore(){
-    var dots = document.getElementById("dots");
-    var more = document.getElementById("more");
-    var btnmore = document.getElementById("btnmore");
-    if(dots.style.display === "none")
-    {
-        dots.style.display="inline";
-        btnmore.innerlHTML="Czytaj więcej";
-        more.style.display="none";
-    }
-    else{
-        dots.style.display="none";
-        btnmore.innerlHTML="Czytaj mniej";
-        more.style.display="inline";
-    }
-}
+function showMore() {
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("btn");
+    
+     if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+  }
+  else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+  }
+  }
