02082b91d40f9f109905105357045ad8fad111e8
Olenakas <121051082+Olenakas@users.noreply.github.com>
2023-05-10 11:15:10 +0200

    спойлер

--- js/more-answer.js
+++ js/more-answer.js
@@ -1,16 +1,28 @@
-const btnAnswers = document.querySelectorAll(".btn-answers");
+// const btnAnswers = document.querySelectorAll(".btn-answers");
 
-btnAnswers.forEach((btn) => {
-  const answer = btn.nextElementSibling;
-  const arrowIcon = btn.querySelector(".arrow-icon");
+// btnAnswers.forEach((btn) => {
+//   const answer = btn.nextElementSibling;
+//   const arrowIcon = btn.querySelector(".arrow-icon");
 
-  btn.addEventListener("click", () => {
-    if (answer.style.display === "none") {
-      answer.style.display = "block";
-      arrowIcon.classList.add("active");
-    } else {
-      answer.style.display = "none";
-      arrowIcon.classList.remove("active");
-    }
+//   btn.addEventListener("click", () => {
+//     if (answer.style.display === "none") {
+//       answer.style.display = "block";
+//       arrowIcon.classList.add("active");
+//     } else {
+//       answer.style.display = "none";
+//       arrowIcon.classList.remove("active");
+//     }
+//   });
+// });
+
+const spoilerList = document.querySelectorAll(".spoiler");
+
+spoilerList.forEach((spoiler) => {
+  spoiler.addEventListener("click", (e) => {
+    e.currentTarget.classList.toggle("show");
+
+    e.currentTarget
+      .querySelector(".spoiler-title-button")
+      .classList.toggle("down");
   });
 });
--- js/more-pn.js
+++ js/more-pn.js
@@ -1,11 +1,15 @@
 function showMorePn() {
   let moreText = document.getElementById("more");
-  let btnText = document.getElementById("btn");
+  let btnText = document.querySelector(".btn-text");
+  let arrowIconLink = document.querySelector(".arrow-icon-link");
+  document.getElementById("btn").classList.toggle("active");
   if (moreText.style.display === "none") {
     btnText.innerHTML = "Czytaj mniej";
     moreText.style.display = "block";
+    arrowIconLink.setAttribute("href", "./images/icons.svg#icon-arrow-up2");
   } else {
     btnText.innerHTML = "Czytaj więcej";
     moreText.style.display = "none";
+    arrowIconLink.setAttribute("href", "./images/icons.svg#icon-arrow-down2");
   }
 }
