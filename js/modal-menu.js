85ea0a6bd41354cbfad0906a086aa8c9e117481a
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-07-23 13:11:50 +0200

    adaptive

--- js/modal-menu.js
+++ js/modal-menu.js
@@ -2,27 +2,41 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuItems = mobileMenu.querySelectorAll('.js-menu-item');
+  const body = document.querySelector('body');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    const isMenuOpen = mobileMenu.classList.contains('is-open');
     mobileMenu.classList.toggle('is-open');
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    
+    if (isMenuOpen) {
+      body.style.overflow = ''; // Восстановить прокрутку контента
+    } else {
+      body.style.overflow = 'hidden'; // Остановить прокрутку контента
+    }
+  };
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    // bodyScrollLock[scrollLockMethod](document.body);
+  const closeMenu = () => {
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    body.style.overflow = ''; // Восстановить прокрутку контента
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    // bodyScrollLock.enableBodyScroll(document.body);
+  menuItems.forEach(menuItem => {
+    menuItem.addEventListener('click', closeMenu);
   });
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  const mediaQuery = window.matchMedia('(min-width: 768px)');
+  const handleScreenChange = e => {
+    if (!e.matches) {
+      closeMenu();
+    }
+  };
+
+  mediaQuery.addEventListener('change', handleScreenChange);
 })();
